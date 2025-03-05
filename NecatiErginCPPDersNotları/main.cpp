4bdd6c31ec8d4f3c12f019c607e096adb61edd25
Emre Türksever <kasimir39100@gmail.com>
2025-03-05 20:15:45 +0300

    .

--- NecatiErginCPPDersNotları/main.cpp
+++ NecatiErginCPPDersNotları/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <string>
-#include <chrono>
-#include <vector>
-#include <optional>
-#include <stdexcept>
-#include <utility>
-	friend std::ostream& operator<<(const std::ostream& os, const Nec nec)
 
 
-int main() {
+int main()
+{
 
 
-    return 0;
+
 }
